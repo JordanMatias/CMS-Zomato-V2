ac69326a1922c3eed3a5242779c11b43cea8dc2b
Jordan <121629654+JordanMatias@users.noreply.github.com>
2023-03-08 19:39:33 -0500

    Update Routing.js

--- src/Routing.js
+++ src/Routing.js
@@ -8,22 +8,6 @@ import RegisterComp from './components/loginRegister/RegisterComp'
 import LoginComp from "./components/loginRegister/LoginComp";
 import Home from './components/Tune'
 
-
-import RestaurantDetailsApi from './components/details/CloudAPI'
-import ViewOrdersApi from './components/veiworders/VendorAPI'
-
-
-const Routing = () => {
-    return (
-      <BrowserRouter>
-        <Route exact path="/" component={Home} />
-        <Route path="/list/:mealId" component={ListingApi} />
-        <Route path="/details/:res_id" component={RestaurantDetailsApi} />
-        <Route path="/viewOrder" component={ViewOrdersApi} />
-        <Route path="/register" component={RegisterComp} />
-        <Route path="/login" component={LoginComp} />
-        <Route path="/viewStripe" component={viewStripe} />
-        <Route path="/registerOrder" component={registerOrder} />
         <Route path="/logout" component={LogoutComp} />
         <Route path="/attributeComp" component={attributeComp} />
         <Route path="/list" component={list} />
